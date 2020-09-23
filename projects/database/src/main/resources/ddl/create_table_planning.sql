4056e64ada1c4394e14ca20a0f0985da0801a35a
egrellis <edwardellis.egre@gmail.com>
2020-09-23 21:43:54 +0100

    Fixed planning schema to include proposal

--- projects/database/src/main/resources/ddl/create_table_planning.sql
+++ projects/database/src/main/resources/ddl/create_table_planning.sql
@@ -2,7 +2,7 @@ CREATE TABLE planning_application (
   name CHAR(12),
   valid_date DATE,
   address VARCHAR(200),
-  proposal VARCHAR(20),
+  proposal VARCHAR(1000),
   type VARCHAR(50),
   status VARCHAR(20),
   ward VARCHAR(20),
