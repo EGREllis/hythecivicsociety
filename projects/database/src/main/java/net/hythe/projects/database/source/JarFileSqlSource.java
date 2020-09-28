ce9a24e93032c360e83b7c4d7777841d4f05e78a
egrellis <edwardellis.egre@gmail.com>
2020-09-28 11:14:37 +0100

    Added a way to read sql from jar file, but it is buggy when the size of the zip entry is not known.

--- projects/database/src/main/java/net/hythe/projects/database/source/JarFileSqlSource.java
+++ projects/database/src/main/java/net/hythe/projects/database/source/JarFileSqlSource.java
@@ -1,5 +1,6 @@
 package net.hythe.projects.database.source;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
@@ -8,6 +9,7 @@ import java.util.zip.ZipInputStream;
 import static net.hythe.projects.database.Util.logException;
 
 public class JarFileSqlSource implements SqlSource {
+    private static final int DEFAULT_BUFFER_SIZE = 1024;
     private final String jarFilePath;
 
     public JarFileSqlSource(String jarFilePath) {
@@ -16,26 +18,63 @@ public class JarFileSqlSource implements SqlSource {
 
     @Override
     public String getSqlFromSource(String resource) {
-        System.out.println(String.format("Reading %1$s from jar file at %2$s", resource, jarFilePath));
+        File jarFile = new File(jarFilePath);
+        System.out.println(String.format("Reading %1$s from jar file at %2$s, which exists? %3$s", resource, jarFile.getAbsolutePath(), Boolean.toString(jarFile.exists())));
         StringBuilder sqlStatements = new StringBuilder();
         try (ZipInputStream input = new ZipInputStream(new FileInputStream(jarFilePath))) {
+            // Skip to the entry
             ZipEntry entry = input.getNextEntry();
-            byte[] buffer = new byte[(int)entry.getSize()];
-            if (entry.getName().equals(resource)) {
-                // Unzip and add to sqlStatements
-                int read = input.read(buffer, 0, (int)entry.getSize());
-                char chars[] = new char[buffer.length/2];
-                for (int i = 0; i < chars.length; i++) {
-                    chars[i] = (char)buffer[i*2];
-                    chars[i] = (char)(chars[i] << 8);
-                    chars[i] = (char)(chars[i] | buffer[i*2 + 1]);
-                }
-                String text = new String(chars);
-                sqlStatements.append(text);
+            while (entry != null && !entry.getName().equals(resource)) {
+                entry = input.getNextEntry();
             }
+
+            if (entry == null) {
+                throw new IllegalStateException(String.format("Could not fine entry %1$s in zip file %2$s", resource, jarFile.getAbsolutePath()));
+            }
+
+            sqlStatements.append(sqlFromZipEntry(input, entry));
         } catch (IOException ioe) {
             logException(ioe);
         }
         return sqlStatements.toString();
+    }
+
+    private String sqlFromZipEntry(ZipInputStream input, ZipEntry entry) throws IOException {
+        String sql;
+        int bufferSize;
+        byte []buffer;
+        char []chars;
+
+        if (entry.getSize() > 0) {
+            System.out.println(String.format("Reading sql from zip entry %1$s using known size zip file strategy (size %2$d)", entry.getName(), entry.getSize()));
+            bufferSize = (int)entry.getSize();
+            buffer = new byte[bufferSize];
+            chars = new char[bufferSize/2];
+            int read = input.read(buffer, 0, bufferSize);
+            for (int i = 0; i < chars.length && i < read; i++) {
+                chars[i] = (char)buffer[i*2];
+                chars[i] = (char)(chars[i] << 8);
+                chars[i] = (char)(chars[i] | buffer[i*2 + 1]);
+            }
+            sql = new String(chars);
+        } else if (entry.getSize() < 0) {
+            System.out.println(String.format("Reading sql from zip entry %1$s using unknown size zip file strategy", entry.getName()));
+            StringBuilder message = new StringBuilder();
+            bufferSize = 2;
+            buffer = new byte[bufferSize];
+            chars = new char[bufferSize/2];
+            while (input.read(buffer, 0, bufferSize) > 0) {
+                chars[0] = (char)buffer[0];
+                chars[0] = (char)(chars[0] << 8);
+                chars[0] = (char)(chars[0] | buffer[1]);
+                message.append(chars[0]);
+                buffer[0] = 0;
+                buffer[1] = 0;
+            }
+            sql = message.toString();
+        } else {
+            sql = "";
+        }
+        return sql;
     }
 }
